2f62d46585a0af8d70102dde516c03cdf7584a54
Anthony <AnthonySgro1995@gmail.com>
2022-05-30 16:52:24 -0400

    changing error catching for texts

--- src/twilio.js
+++ src/twilio.js
@@ -1,40 +1,45 @@
-const sendText = (to, from, body) => {
+const sendText = async (to, from, body) => {
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_ACCOUNT_TOKEN;
     const client = require("twilio")(accountSid, authToken);
 
-    client.messages
-        .create({
+    try {
+        const message = await client.messages.create({
             body,
             from,
             to,
-        })
-        .then((message) =>
-            console.log(`Message sent with Twilio:\n${message.sid}`),
-        )
-        .catch((err) => console.error(err));
+        });
+
+        console.log(`Message sent with Twilio:\n${message.sid}`);
+    } catch (err) {
+        throw err;
+    }
 };
 
-module.exports = emitNewListingsViaText = (newListings) => {
-    const isProd = process.env.APP_ENV === "prod";
-    const dev_subscribers = [process.env.TWILIO_MY_PHONE_NUMBER];
-    const prod_subscribers = [
-        process.env.TWILIO_ACCOUNT_MY_PHONE_NUMBER,
-        process.env.TWILIO_ACCOUNT_FRIEND_PHONE_NUMBER,
-    ];
+module.exports = emitNewListingsViaText = async (newListings) => {
+    try {
+        const isProd = process.env.APP_ENV === "prod";
+        const dev_subscribers = [process.env.TWILIO_MY_PHONE_NUMBER];
+        const prod_subscribers = [
+            process.env.TWILIO_ACCOUNT_MY_PHONE_NUMBER,
+            process.env.TWILIO_ACCOUNT_FRIEND_PHONE_NUMBER,
+        ];
 
-    if (newListings.length > 0) {
-        const subscribers = isProd ? prod_subscribers : dev_subscribers;
-        for (let i = 0; i < subscribers.length; i++) {
-            for (const { site, url, address } of newListings) {
-                sendText(
-                    subscribers[i],
-                    process.env.TWILIO_ACCOUNT_PHONE_NUMBER,
-                    `There is a new ${site} Listing!\n${url}`,
-                );
+        if (newListings.length > 0) {
+            const subscribers = isProd ? prod_subscribers : dev_subscribers;
+            for (let i = 0; i < subscribers.length; i++) {
+                for (const { site, url, address } of newListings) {
+                    await sendText(
+                        subscribers[i],
+                        process.env.TWILIO_ACCOUNT_PHONE_NUMBER,
+                        `There is a new ${site} Listing!\n${url}`,
+                    );
+                }
             }
+        } else {
+            console.log("Nothing to text");
         }
-    } else {
-        console.log("Nothing to text");
+    } catch (err) {
+        throw err;
     }
 };
