80d0caec4504db0d0e2ff371c20b65f9b07b6991
Anthony <AnthonySgro1995@gmail.com>
2022-05-30 02:25:54 -0400

    added twilio refactoring

--- src/twilio.js
+++ src/twilio.js
@@ -1,3 +1,8 @@
+const subscribers = [
+    process.env.TWILIO_MY_PHONE_NUMBER,
+    process.env.TWILIO_FRIEND_PHONE_NUMBER,
+];
+
 const sendText = (to, from, body) => {
     const accountSid = process.env.TWILIO_ACCOUNT_SID;
     const authToken = process.env.TWILIO_ACCOUNT_TOKEN;
@@ -19,20 +24,14 @@ const sendText = (to, from, body) => {
 
 module.exports = emitNewListingsViaText = (newListings) => {
     if (newListings.length > 0 && process.env.APP_ENV === "prod") {
-        for (const { site, url, address } of newRentals) {
-            sendText(
-                process.env.MY_PHONE_NUMBER,
-                process.env.TWILIO_ACCOUNT_PHONE_NUMBER,
-                `There is a new ${site} Listing!\n${url}`,
-            );
-
-            sendText(
-                process.env.FRIEND_PHONE_NUMBER,
-                process.env.TWILIO_ACCOUNT_PHONE_NUMBER,
-                `There is a new ${site} Listing!\n${url}`,
-            );
-
-            console.log("Texted Listing to Subscribers:", address);
+        for (let i = 0; i < subscribers.length; i++) {
+            for (const { site, url, address } of newRentals) {
+                sendText(
+                    subscribers[i],
+                    process.env.TWILIO_ACCOUNT_PHONE_NUMBER,
+                    `There is a new ${site} Listing!\n${url}`,
+                );
+            }
         }
     } else {
         console.log("Nothing to text");
